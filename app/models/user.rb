e6172de0e2b1024659ed33e8cfc7908333d9f938
Ryunosuke_Ozaki <138109860+ryunosuke1990127@users.noreply.github.com>
2023-07-27 12:18:32 +0000

    update2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :profile_image
+
+  # モデル側でメソッドを定義しView側で呼び出しが可能なようにする
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    # メソッドの引数に対して画像のサイズ設定を行う
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # ユーザーのプロフィール設定用のルーティング
+  resources :users, only: [:show, :edit]
+  # 本の投稿。削除等のルーティング
+  resources :books, only[:new, :create, :index, :show, :destroy]
+  # deviseGemで機能している認証設定など用のルーティング
   devise_for :users
   # ルートページの設定
   root to: "homes#top"
